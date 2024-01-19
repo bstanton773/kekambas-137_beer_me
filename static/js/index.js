e47fa807eb0e746a6b0178b0adff5ed1e7615c3a
Brian <bstanton773@gmail.com>
2024-01-19 16:09:27 -0600

    Add event listeners on the nav link to set up SPA

--- static/js/index.js
+++ static/js/index.js
@@ -13,6 +13,13 @@ function pageLoader(){
     for (let btn of colorButtons){
         btn.addEventListener('click', changeBackgroundColor)
     }
+
+    // Get the nav links and add the changeView event listener on click
+    let navLinks = document.getElementsByClassName('nav-link');
+    for (let link of navLinks){
+        link.addEventListener('click', changeView)
+    }
+
 }
 
 
@@ -25,4 +32,21 @@ function changeBackgroundColor(e){
     } else {
         document.body.style.backgroundColor = '#FFF897'
     }
+}
+
+// Create a function to make this a Single Page App (SPA) by swapping the divs
+function changeView(e){
+    // turn off the element(s) that are visible
+    const toTurnOff = document.getElementsByClassName('is-visible');
+    for (let element of toTurnOff){
+        element.classList.replace('is-visible', 'is-invisible');
+        // get the nav link associated
+        let navLink = document.getElementsByName(element.id)[0];
+        navLink.classList.remove('active');
+    }
+    // Turn on the element based on the link we clicked
+    let idToTurnOn = e.target.name;
+    const toTurnOn = document.getElementById(idToTurnOn);
+    toTurnOn.classList.replace('is-invisible', 'is-visible');
+    e.target.classList.add('active');
 }
